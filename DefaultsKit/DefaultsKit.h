ef2a0eb419bf453cf56e4ce55e9088e5e253d218
Nuno Dias <nmdias.pt@gmail.com>
2017-08-15 20:38:39 +0100

    Initial Commit

---
+++ DefaultsKit/DefaultsKit.h
@@ -0,0 +1,19 @@
+//
+//  DefaultsKit.h
+//  DefaultsKit
+//
+//  Created by Nuno Dias on 15/08/2017.
+//  Copyright © 2017 Nuno Dias. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DefaultsKit.
+FOUNDATION_EXPORT double DefaultsKitVersionNumber;
+
+//! Project version string for DefaultsKit.
+FOUNDATION_EXPORT const unsigned char DefaultsKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DefaultsKit/PublicHeader.h>
+
+
