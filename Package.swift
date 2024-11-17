a45a9ab52033aec368fbfe563c8f10a90e14c398
Nuno Dias <nmdias@users.noreply.github.com>
2024-11-17 09:47:04 +0100

    Update package to swift 6.0

--- Package.swift
+++ Package.swift
@@ -1,15 +1,23 @@
-// swift-tools-version:4.0
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 
 let package = Package(
-    name: "DefaultsKit",
-    products: [
-        .library(name: "DefaultsKit", targets: ["DefaultsKit"]),
-    ],
-    targets: [
-        .target(
-            name: "DefaultsKit",
-            path: "Sources"
-        ),
-    ]
+  name: "DefaultsKit",
+  products: [
+    .library(
+      name: "DefaultsKit",
+      targets: ["DefaultsKit"]
+    ),
+  ],
+  targets: [
+    .target(
+      name: "DefaultsKit"
+    ),
+    .testTarget(
+      name: "DefaultsKitTests",
+      dependencies: ["DefaultsKit"]
+    ),
+  ]
 )
