7b8fb8f37b51165c3c096012367e4b1e153e2811
Nuno Dias <nmdias@users.noreply.github.com>
2024-11-17 10:01:22 +0100

    Update copyright dates

--- Tests/DefaultsTests.swift
+++ Tests/DefaultsTests.swift
@@ -1,7 +1,7 @@
 //
 //  DefaultsTests.swift
 //
-//  Copyright (c) 2017 - 2018 Nuno Manuel Dias
+//  Copyright (c) 2017 - 204 Nuno Manuel Dias
 //
 //  Permission is hereby granted, free of charge, to any person obtaining a copy
 //  of this software and associated documentation files (the "Software"), to deal
--- Tests/RawRepresentableMock.swift
+++ Tests/RawRepresentableMock.swift
@@ -1,7 +1,7 @@
 //
 //  PersonMock.swift
 //
-//  Copyright (c) 2017 - 2018 Nuno Manuel Dias
+//  Copyright (c) 2017 - 2024 Nuno Manuel Dias
 //
 //  Permission is hereby granted, free of charge, to any person obtaining a copy
 //  of this software and associated documentation files (the "Software"), to deal
